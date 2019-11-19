3227038f8fd5b1feb8d984e5ff32a5270b7bb84c
Chris Laprun <metacosm@gmail.com>
2019-11-19 14:55:45 +0100

    fix: update to latest abstract-operator and kotlin versions

--- src/main/kotlin/io/radanalytics/operator/kotlin/BarOperator.kt
+++ src/main/kotlin/io/radanalytics/operator/kotlin/BarOperator.kt
@@ -1,13 +1,11 @@
 package io.radanalytics.operator.kotlin
 
-import io.radanalytics.operator.common.*
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
+import io.radanalytics.operator.common.AbstractOperator
+import io.radanalytics.operator.common.EntityInfo
+import io.radanalytics.operator.common.Operator
 
-@Operator(forKind = "bar", prefix = "radanalytics.io", infoClass = BarInfo::class)
+@Operator(forKind = BarInfo::class, prefix = "radanalytics.io")
 class BarOperator : AbstractOperator<BarInfo>() {
-
-    val log: Logger = LoggerFactory.getLogger(BarOperator::class.qualifiedName)
 
     override fun onDelete(bar: BarInfo) {
         log.info("deleted 'bar' with name ${bar.name} and parameter = ${bar.parameter}")
@@ -18,9 +16,7 @@ class BarOperator : AbstractOperator<BarInfo>() {
     }
 }
 
-class BarInfo(var nameValue: String, var parameter: String) : EntityInfo {
-
-    constructor() : this("", "")
+class BarInfo(var nameValue: String, var parameter: String) : EntityInfo() {
 
     override fun setName(value: String) {
         nameValue = value
